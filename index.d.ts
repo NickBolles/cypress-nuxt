cb8ccfa721f96723340d617f93439bbaf70438fd
Nick Bolles <me@nickbolles.com>
2020-04-06 23:12:54 -0500

    feat: add typings for transform and promise result
    Requires https://github.com/cypress-io/cypress-webpack-preprocessor/pull/72

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,22 @@
+import {CypressWebpackPreProcessor} from '@cypress/webpack-preprocessor';
+/**
+ * Options for Cypress-nuxt
+ */
 interface Options {
-    transform(webpackConfig: any): Promise<any> | any;
+    /**
+     * A hook to transform the webpack config
+     * 
+     * @param webpackConfig The original configuration that nuxt returned
+     * @returns actual webpack config to use
+     */
+    transform(webpackConfig: CypressWebpackPreProcessor.Options): Promise<CypressWebpackPreProcessor.Options> | CypressWebpackPreProcessor.Options;
 }
 
-export function plugin(opts?: Options): Promise<any>;
+/**
+ * Cypress plugin that will return a file preprocessor for nuxt.
+ * This allows one liner nuxt configuration for Cypress in order to unit test components.
+ * 
+ * @param opts Options for the plugin
+ * @returns - Promise that resolves in a file pre-processor (from `@cypress/webpack-preprocessor`) that can be used in `on("file:preprocessor")` calls
+ */
+export function plugin(opts?: Options): Promise<CypressWebpackPreProcessor.FilePreprocessor>;
